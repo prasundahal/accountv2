bdccc85fd82313acfffa75bf38aea0f0c2b5ba3a
JoshiBipin <bipin.ingrails@gmail.com>
2022-08-30 01:42:23 +0545

    cronjob for mail of spinner links on 3rd

--- resources/views/newLayout/layouts/newLayout.blade.php
+++ resources/views/newLayout/layouts/newLayout.blade.php
@@ -99,7 +99,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link {{ (request()->segment(1) == 'redeem-history') ? 'active' : '' }}" href="{{route('redeem-history')}}">
+                <a class="nav-link {{ (request()->segment(1) == 'redeem-history') ? 'active' : '' }}" href="{{route('redeemHistory')}}">
                   <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                     <i class="fa fa-history text-warning text-sm opacity-10"></i>
                   </div>
