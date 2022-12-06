431d51a4bcdd8a36039cb1746193f03a02d297b6
JoshiBipin <bipin.ingrails@gmail.com>
2022-12-07 00:06:34 +0545

    inactive mail cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,25 +36,27 @@ class Kernel extends ConsoleKernel
         //  $schedule->command('colabUpdate:cron')->everyMinute();
         //  $schedule->command('DailyReport:cron')->everyMinute();
         // Log::channel('cronLog')->info(date('H:i'));
-         $schedule->command('colabUpdate:cron')
-         ->daily();
-         $schedule->command('DailyReport:cron')
-         ->daily();
-         $schedule->command('SpinnerResetForm:cron')
-         ->monthlyOn(26, '00:00');
-         $schedule->command('sendMailToBetween:cron')
-         ->monthlyOn(20, '00:00');
-         $schedule->command('SpinnerMailToAboveLimit:cron')
-         ->monthlyOn(5, '00:00');
-         $schedule->command('MonthlyTasks:cron')
-         ->monthlyOn(26, '00:00');
-         $schedule->command('SpinnerWinnerCron:cron')
-         ->monthlyOn($setting->spinner_winner_day,date('H:i',strtotime($setting->spinner_time_cron)));
+        //  $schedule->command('colabUpdate:cron')
+        //  ->daily();
+        //  $schedule->command('DailyReport:cron')
+        //  ->daily();
+        //  $schedule->command('SpinnerResetForm:cron')
+        //  ->monthlyOn(26, '00:00');
+        //  $schedule->command('sendMailToBetween:cron')
+        //  ->monthlyOn(20, '00:00');
+        //  $schedule->command('SpinnerMailToAboveLimit:cron')
+        //  ->monthlyOn(5, '00:00');
+        //  $schedule->command('MonthlyTasks:cron')
+        //  ->monthlyOn(26, '00:00');
+        //  $schedule->command('SpinnerWinnerCron:cron')
+        //  ->monthlyOn($setting->spinner_winner_day,date('H:i',strtotime($setting->spinner_time_cron)));
         
          $type = $setting->inactive_mail_type;
          if($setting->inactive_mail_type == 'dailyAt' || $setting->inactive_mail_type == 'lastDayOfMonth'){
             $schedule->command('InactiveMail:cron')->$type($setting->inactive_mail_time);            
-         }else{
+         }elseif($setting->inactive_mail_type == 'everyMinute'){
+            $schedule->command('InactiveMail:cron')->everyMinute();
+        }else{
             $schedule->command('InactiveMail:cron')->$type($setting->inactive_mail_day,date('H:i',strtotime($setting->inactive_mail_time)));
          }
         // $schedule->command('MonthlyTasks:cron')
